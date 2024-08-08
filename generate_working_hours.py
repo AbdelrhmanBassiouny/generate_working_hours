dbd312443b809f61fdb04192a2fec19ee9510daa
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2024-08-08 17:26:25 +0200

    some cleaning

--- generate_working_hours.py
+++ generate_working_hours.py
@@ -1,13 +1,12 @@
 import numpy as np
-from typing import List
 
 low = 6  # min hours per day
 high = 10  # max hours per day
-possible_values: List = np.arange(low, high, 0.25).tolist()
+possible_values = np.arange(low, high, 0.25)
 n_months = 7
 vacation_days = [10, 0, 1, 13, 3, 4, 0]
 days_per_month = [23, 21, 21, 22, 23, 20, 23]
-tolerance_for_total_hours_of_whole_period = 8
+tolerance_for_total_hours_of_whole_period = 0.25
 
 while True:
     all_daily_working_hours = []
@@ -20,13 +19,18 @@ while True:
             hours_per_day = possible_values[hours_per_day_idx]
             per_month_daily_working_hours.append(hours_per_day)
             all_daily_working_hours.append(hours_per_day)
+
         all_monthly_working_hours.append(per_month_daily_working_hours)
+
     print(sum(all_daily_working_hours), "should be close to", 143*n_months,
           "error:", sum(all_daily_working_hours) - 143*n_months)
+
     if np.isclose(sum(all_daily_working_hours), 143*n_months, atol=tolerance_for_total_hours_of_whole_period):
         break
 
 # print(all_daily_working_hours)
 print(sum(all_daily_working_hours))
-arr = np.array(all_monthly_working_hours)
-print(arr)
+for row in all_monthly_working_hours:
+    print(', '.join(str(x) for x in row))
+# arr = np.array(all_monthly_working_hours)
+# print(arr)
